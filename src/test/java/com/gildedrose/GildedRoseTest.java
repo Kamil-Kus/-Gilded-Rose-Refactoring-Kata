8193ca347c55a862e89de1b676f7b92d9e30b297
Kamil Kus <KamilKusDev@protonmail.com>
2022-05-08 15:39:28 +0200

    Added tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -4,11 +4,27 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.approvaltests.combinations.CombinationApprovals;
+
 class GildedRoseTest {
 
     @Test
+    void testUpdateOfQuality() {
+        CombinationApprovals.verifyAllCombinations(this::computeItem, new String[]{"foo", "Aged Brie", "Elixir of the Mongoose", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros", "+5 Dexterity Vest"},
+                new Integer[]{0, 2, 3, 4, 20, 99, 200},
+                new Integer[]{0, 50, 1, 2, 3, 34, 12});
+    }
+
+    private String computeItem(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        return app.items[0].toString();
+    }
+
+    @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
